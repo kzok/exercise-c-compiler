a68d55bc77ea0c3269cf48d9b127bf37cac314d3
kzok <yes.notyet@gmail.com>
2020-09-13 08:08:05 +0000

    refactor: test.h

--- test.sh
+++ test.sh
@@ -29,28 +29,38 @@ try() {
 	fi
 }
 
+# statement
 try 0 "0;"
 try 42 "42;"
+
+# calculation
 try 21 "5+20-4;"
 try 41 " 12 + 34    - 5;"
 try 47 "5+6*7;"
 try 15 "5 * (9 - 6);"
 try 4 "(3   + 5) / 2;"
 try 10 "-  10 +20;"
+
+# comparison
 try 0 "1 > 2;"
 try 1 "1 < 2;"
 try 0 "2 < 2;"
 try 1 "2 <= 2;"
 try 0 "2 <= 1;"
+
+# variable
 try 3 "a = 1; b = a + 2; b;"
 try 6 "foo = 1; bar = 2 + 3; foo + bar;"
+
+# return
+try 3 "return 3;"
 try 5 "return 5; return 8;"
 
+# if, else
 try 2 "if ( 1 ) return 2 ; return 3 ;"
 try 3 "if ( 0 ) return 2 ; return 3 ;"
 try 4 "a=1; if(1)a=a+3; if(0)a=a+3; a;"
-
 try 1 "a=0; if ( 1 ) a=1; else a=2; a;"
 try 2 "a=0; if ( 0 ) a=1; else a=2; a;"
 
-echo OK
+echo -e '\e[32mAll tests passed!\e[0m'
