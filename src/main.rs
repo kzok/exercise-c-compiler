bea9b576cb8fedecc7bd8b9096642f893bf06702
kzok <yes.notyet@gmail.com>
2020-09-30 13:29:10 +0000

    ステップ 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn main()  {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("引数の個数が正しくありません");
+        std::process::exit(1);
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("\tmov rax, {}", args[1].parse::<i32>().unwrap());
+    println!("\tret");
+    std::process::exit(0);
 }
